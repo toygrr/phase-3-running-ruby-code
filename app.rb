2841dc7554c5e9ad75516893285c33692095dc87
Tyler <toygrrr@gmail.com>
2022-08-26 12:42:08 -0700

    commit tree 16d13681afaebcb7a0c29745607c66128e897df8

---
+++ app.rb
@@ -0,0 +1,30 @@
+# #app.rb
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
+
+# puts "Hello world!"  #equivalent to console.log - the first syntax is preferred
+# puts("Hello world!")
+
+# # run code in terminal by using "ruby <filename>"
+# # puts will automatically add a line break between strings, whereas print will not.
+
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+
+# # puts isnt ideal for displaying every data type into the console..
+# # in this case, each value will be displayed on its own line. (calls .to_s method, coercing it into a string)
+# puts [1, 2, 3]
+
+# # the p method will return the whole array on less lines (calls the inspect method)
+# p [1, 2, 3]
+
+# # pp 'pretty printing' will display the information in the console but colorized and pretty, for more complex data types
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+# # all methods in ruby have a return value
+# # the puts method always returns nil (null in JS)
+# #always use snake_case when writing seperate variable words.
+
